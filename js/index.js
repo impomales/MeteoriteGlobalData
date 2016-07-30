732f154989cccc190a489261644c702fe601fff7
impomales <impomales@gmail.com>
2016-07-29 22:47:03 -0400

    tooltip showing meteortie info

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var width = '100%';
-  var height = 650;
+  var height = 600;
   var worldURL = 'https://raw.githubusercontent.com/mbostock/topojson/master/examples/world-110m.json';
   var meteoriteJSON = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json';
   var nasaJSON = 'https://data.nasa.gov/resource/y77d-th95.geojson';
@@ -16,6 +16,14 @@ $(document).ready(function() {
   
   var g = svg.append("g");
   
+  var tip = d3.tip()
+  .attr('class', 'tip')
+  .html(function(d) {
+    return "<p><b>fall: </b>" + d.properties.fall + "</p><p><b>mass: </b>" + d.properties.mass + "</p><p><b>name: </b>" + d.properties.name + "</p><p><b>nametype: </b>" + d.properties.nametype + "</p><p><b>recclass: </b>" + d.properties.recclass + "</p><p><b>reclat: </b>" + d.properties.reclat + "</p><p><b>year: </b>" + d.properties.year + "</p>";
+  });
+  
+  g.call(tip);
+  
   d3.json(worldURL, function(error, topology) {
     g.selectAll("path")
       .data(topojson.feature(topology, topology.objects.countries)
@@ -24,8 +32,6 @@ $(document).ready(function() {
         .attr('d', path);
     
     d3.json(nasaJSON, function(error, data) {
-      var max = d3.max(data.features.map(function(d) {return parseFloat(d.properties.mass);}));
-      console.log(max);
       g.selectAll("circle")
         .data(data.features)
         .enter().append('circle')
@@ -36,14 +42,16 @@ $(document).ready(function() {
         .attr('cy', function(d) {
           return projection([d.properties.reclong, d.properties.reclat])[1];})
         .attr('r', function(d) {
-          var mass = parseFloat(d.properties.mass);
+          var mass = d.properties.mass;
           if (mass < 100000) return 2;
           if (mass < 200000) return 5;
           if (mass < 300000) return 10;
           if (mass < 2000000) return 15;
           if (mass < 10000000) return 20;
           return 25;
-        });
+        })
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
     }); 
   });
 });
